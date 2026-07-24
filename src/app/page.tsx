6f9cabaa7de6fb246b549fd1ed62bb8dd9e51b36
Abdi Biranu Tolasa <Abdi.Biranu@gohbetbank.com>
2026-07-24 16:36:54 +0300

    Shorten homepage content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 import { Navigation } from '@/components/Navigation';
 import { Hero } from '@/components/Hero';
-import { About } from '@/components/About';
 import { HomeHighlights } from '@/components/HomeHighlights';
-import { Portfolio } from '@/components/Portfolio';
-import { Technologies } from '@/components/Technologies';
-import { WhyChooseUs } from '@/components/WhyChooseUs';
-import { Contact } from '@/components/Contact';
 import { Footer } from '@/components/Footer';
 
 export default function Home() {
@@ -13,12 +8,7 @@ export default function Home() {
     <main className="min-h-screen">
       <Navigation />
       <Hero />
-      <About />
       <HomeHighlights />
-      <Portfolio />
-      <Technologies />
-      <WhyChooseUs />
-      <Contact />
       <Footer />
     </main>
   );
